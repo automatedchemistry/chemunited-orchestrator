1286cd158b88914c7650f622f77287c34996a49d
Samuel Vitor Saraiva <samuk_saraiva@hotmail.com>
2026-04-13 17:10:20 +0200

    feat(syringepump): add SVG layer for syringe components and enhance build method
    - Implemented SVG layer for syringe plunger and barrel in SyringePump class.
    - Updated build method to include new SVG components and adjusted positioning.

--- src/chemunited/qt/elements/component/glossary/pumps/syringepump_graph.py
+++ src/chemunited/qt/elements/component/glossary/pumps/syringepump_graph.py
@@ -1,9 +1,26 @@
-from typing import ClassVar
-
 from chemunited.core.components import FlowSourceData, FlowSourceMode
 from chemunited.qt.elements.component.graph_item import GraphComponent
+from chemunited.qt.elements.component.component_parts.svg_layer import SvgLayer
+from chemunited.core.common.constant import PATTERN_DIMENSION
+from PyQt5.QtCore import QFile
+from typing import ClassVar
+
 
 
 class SyringePump(GraphComponent[FlowSourceData]):
     METADATA: ClassVar[type[FlowSourceData]] = FlowSourceData
     BASEMODE: ClassVar[type[FlowSourceMode]] = FlowSourceMode
+
+    def build(self) -> None:
+        svg_path = f":/components_icons/components/SyringePlunger.svg"
+        if QFile.exists(svg_path):
+            self._syringe_plunger = SvgLayer(
+                svg_path,
+                angle=self._data.angle,
+                scale=PATTERN_DIMENSION * 1.2,
+                parent=self,
+            )
+            self._syringe_plunger.moveBy(2.5, 11.5)
+            self.addToGroup(self._syringe_plunger)
+        super().build(svg_path=f":/components_icons/components/SyringeBarrel.svg")
+        
