7fe7cf2bc768cf6ea0f1c251c8235069ac244007
Samuel Vitor Saraiva <samuk_saraiva@hotmail.com>
2026-04-14 19:34:02 +0200

    feat: add SVG assets and graph component implementations for flow meter, pressure sensor, and bubble sensor

--- src/chemunited/qt/elements/component/glossary/sensors/flowmeter_graph.py
+++ src/chemunited/qt/elements/component/glossary/sensors/flowmeter_graph.py
@@ -7,4 +7,9 @@ class FlowMeter(GraphComponent[ComponentData]):
     METADATA: ClassVar[type[ComponentData]] = ComponentData
     BASEMODE: ClassVar[type[ComponentMode]] = ComponentMode
 
+    def build(self, svg_path: str | None = None) -> None:
+        self._data.ports_by_number[1].relative_position = (42, 22)
+        self._data.ports_by_number[2].relative_position = (-42, 22)
+        return super().build(svg_path=f":/components_icons/components/FlowMeter.svg")
+
     
